7ee310f7795c77d3aac5284c30ba47fb1b8fdee5
月光下的光头 <jiege82000@163.com>
2015-06-02 13:12:46 +0800

    修改功能

--- src/main/resources/git.sh
+++ src/main/resources/git.sh
@@ -2,6 +2,13 @@ cd /Users/xiayingjie/Documents/project/java/xyj
 git  init
 git  add .
 git commit -m '第一次提交'
-git remote add origin https://github.com/xiayingjie/common.git
-git push -u origin master
+git remote add com https://github.com/xiayingjie/com.git
+git push -u com master
 
+
+
+cd /Users/xiayingjie/Documents/project/java/xyj
+git  add .
+git commit -m '修改功能'
+git remote add com-github https://github.com/xiayingjie/com.git
+git push -u com-github master
