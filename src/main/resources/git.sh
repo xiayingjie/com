5fc01ab43f5da69b3f0809c05583431f378b6764
月光下的光头 <jiege82000@163.com>
2015-06-02 12:50:28 +0800

    提交信息

--- src/main/resources/git.sh
+++ src/main/resources/git.sh
@@ -2,6 +2,6 @@ cd /Users/xiayingjie/Documents/project/java/xyj
 git  init
 git  add .
 git commit -m '第一次提交'
-git remote add common-github https://github.com/xiayingjie/common.git
-git push -u common-github master
+git remote add origin https://github.com/xiayingjie/common.git
+git push -u origin master
 
